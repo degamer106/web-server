65632a374dd966cd90bc61836430456fb4d9c761
Allen Lin <allen.lin@trinet.com>
2015-11-22 20:02:36 -0800

    Update port for heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 // Create an express instance
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 var middleware = require('./middleware');
 
